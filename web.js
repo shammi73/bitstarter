35c3faa386631e06af6f92bafe12250187d897c8
sam <shammi.nith@gmail.com>
2013-07-12 07:50:04 +0530

    ya

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
+var fs = require("fs");
+var buffer = new Buffer(fs.readFileSync("/home/sam/bitstater/index.html","utf-8"));
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString("utf-8",0,10));
+
 });
 
 var port = process.env.PORT || 5000;
