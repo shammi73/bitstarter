d02f0b3fa9953fd6567ff86ff0d7fc1056a642ab
sam <shammi.nith@gmail.com>
2013-07-12 09:07:55 +0530

    aaa

--- web.js
+++ web.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var fs = require("fs");
-var buffer = new Buffer(fs.readFileSync("index.html","utf-8"));
+var buffer = new Buffer(fs.readFileSync("index.html", "utf-8"));
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString("utf-8"));
-
+  response.send( buffer.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
